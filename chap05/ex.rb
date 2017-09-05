9d882b531b7eda769d2427201850ce2b519ba2d2
Georgios <george.lamprakis@gmail.com>
2017-09-05 10:16:47 +0100

    Chapter 5 files added

---
+++ chap05/ex.rb
@@ -0,0 +1,8 @@
+#5.6 p28
+puts "Please input your first name"
+first_name = gets.chomp.capitalize
+puts "Please input your middle name"
+middle_name = gets.chomp.capitalize
+puts "Please input your last name"
+last_name = gets.chomp.capitalize
+puts "Hello #{first_name} #{middle_name} #{last_name}!"
