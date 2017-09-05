473c2377e017afd43ccc9c03523a2b84017c7e6c
Georgios <george.lamprakis@gmail.com>
2017-09-05 10:17:24 +0100

    Chapter 6 files added

---
+++ chap06/ex1.rb
@@ -0,0 +1,3 @@
+#6.2 p36  Angry boss 
+puts "What do you want???"
+puts "WHADDAYA MEAN \"#{gets.chomp.upcase}\"?!? YOU 'RE FIRED!!"
---
+++ chap06/ex2.rb
@@ -0,0 +1,6 @@
+#6.2 p36 Recreate the table of the the book
+pw = 70
+puts "Table of Contents".center(pw)
+puts "Chapter 1: Getting Started".ljust(pw/2) +"page  1".rjust(pw/2)
+puts "Chapter 2: Numbers".ljust(pw/2) + "page  9".rjust(pw/2)
+puts "Chapter 3: Letters".ljust(pw/2) + "page 13".rjust(pw/2)
